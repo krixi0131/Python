ccba02c8047cf9970b517e7f706701aba9fd84bc
ann <annnna6633@gmail.com>
2022-06-17 21:14:16 +0800

    'nqeen'

--- test.py
+++ test.py
@@ -1,16 +1,27 @@
-numlist=list(map(int,input().split()))
-flag=0 #團體數
-a=[] #同學統計過與否
-friend=[int(x) for x in numlist] #好友編號
-a = [0 for i in range(len(friend))]
-for j in range(len(friend)):
-    if a[j]==0: #從沒算的開始算，算過的變為1
-        ff=j #暫存數
-        t=[]
-        while a[ff]==0:#沒算過就繼續算       
-            t.append(ff)
-            a[ff]=1 #算過的變為1
-            ff=friend[ff] #一直循環到成一團體       
-        print(*t)
-        flag+=1 #團體數+1
-print('小團體數:',flag)
+def rr(n):
+    return know(n,0,[True for i in range(n)],[True for i in range(2*n-1)],[True for i in range(2*n-1)],[[0 for i in range(n)]for i in range(n)])
+#n=皇后數量
+#a=負責編號
+#b直線是否可放皇后
+#c右上
+#d右下
+def know(n,a,b,c,d,picture):
+    if n == a : #已經擺上n個皇后
+        for i in picture:
+            print(*i)
+        print("---------------")
+    for k in range(n): #k去試每一欄
+        if b[k] and c[a+k] and d[a-k+n-1]: #直、右上斜、右下斜 
+            picture[a][k]=1
+            b[k],c[a+k],d[a-k+n-1]=False,False,False #直、右上斜、右下斜都不能放
+            know(n,a+1,b,c,d,picture)
+            b[k],c[a+k],d[a-k+n-1]=True,True,True #恢復初始資料
+            picture[a][k]=0
+
+def main():
+    n=int(input())
+    if n == 2 or n == 3:
+        print('找不到解')
+    rr(n)
+    
+main()
