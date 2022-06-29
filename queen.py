2e08dacc132096104d458eb942b74a61c9ff7b3e
ann <annnna6633@gmail.com>
2022-06-29 19:43:30 +0800

    'ab'

--- queen.py
+++ queen.py
@@ -1,26 +1,51 @@
-def rr(n):
-    return know(n,0,[True for i in range(n)],[True for i in range(2*n-1)],[True for i in range(2*n-1)],[[0 for i in range(n)]for i in range(n)])
-#n=皇后數量
-#a=負責編號
-#b直線是否可放皇后
-#c右上
-#d右下
-def know(n,a,b,c,d,picture):
-    if n == a : #已經擺上n個皇后
-        for i in picture :
-            print(*i)
-        print("---------------")
-    for k in range(n): #k去試每一欄
-        if b[k] and c[a+k] and d[a-k+n-1]: #直、右上斜、右下斜 
-            picture[a][k]=1
-            b[k],c[a+k],d[a-k+n-1]=False,False,False #直、右上斜、右下斜都不能放
-            know(n,a+1,b,c,d,picture)
-            b[k],c[a+k],d[a-k+n-1]=True,True,True #恢復初始資料
-            picture[a][k]=0
+def xx(numx,listx):
+    if numx.count('?')==0: #x.count=計算X有幾個問號
+        listx.append(numx) #數字放進list
+    for i in range(len(numx)): #把numx每個字元跑一次    
+        if numx[i]=='?':
+            for j in range(10):
+                tmp=numx
+                tmp=tmp[:i]+str(j)+tmp[i+1:]  # replace tmp[i] with str(j)
+                xx(tmp,listx)
+            break
+
+def yy(numy,listy): 
+    if numy.count('?')==0: 
+        listy.append(numy) 
+    for i in range(len(numy)):     
+        if numy[i]=='?':
+            for j in range(10):
+                tmp=numy
+                tmp=tmp[:i]+str(j)+tmp[i+1:] 
+                yy(tmp,listy)
+            break
+ 
+def zz(numz,listz):
+    if numz.count('?')==0: 
+        listz.append(numz) 
+    for i in range(len(numz)):   
+        if numz[i]=='?':
+            for j in range(10):
+                tmp=numz
+                tmp=tmp[:i]+str(j)+tmp[i+1:]  
+                zz(tmp,listz)
+            break
+            
+def jia(listx,listy,listz):  #x+y=z的話就印出
+    for i in range(len(listx)):
+        for ii in range(len(listy)):
+            ans=str(int(listx[i])+int(listy[ii]))
+            if ans in listz:
+                print(int(listx[i]),int(listy[ii]),int(listx[i])+int(listy[ii]))
+              
 def main():
-    n=int(input())
-    if n == 2 or n == 3:
-        print('找不到解')
-    rr(n)
-    
+    numx,numy,numz=map(str,input().split()) 
+    listx=[]
+    listy=[]
+    listz=[]
+    xx(numx,listx)
+    yy(numy,listy)
+    zz(numz,listz)
+    jia(listx,listy,listz)
+
 main()
